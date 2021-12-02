3140ce65f0dfc083d82325703bc5d43b9b932ade
Vladimir <vlakuva@gmail.com>
2021-12-02 08:50:38 +0300

    lint for prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -8,31 +8,31 @@ const question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 const getRandomNumber = (min, max) => +Math.floor(Math.random() * (max - min)) + min;
 
 const isPrime = (number) => {
-    if (number === 2) {
-        return true;
-    }
-
-    if (number < 2 || number % 2 === 0) {
-        return false;
-    }
-
-    for (let i = 2; i <= Math.ceil(Math.sqrt(number)); i += 1) {
-        if (number % i === 0) {
-            return false;
-        }
-    }
-
+  if (number === 2) {
     return true;
+  }
+
+  if (number < 2 || number % 2 === 0) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.ceil(Math.sqrt(number)); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 const runPrimeGame = () => {
-    const getDataForRound = () => {
-        const number = getRandomNumber(MIN_VALUE, MAX_VALUE);
-        const rightAnswer = isPrime(number) ? 'yes' : 'no';
-        return [number, rightAnswer];
-    };
+  const getDataForRound = () => {
+    const number = getRandomNumber(MIN_VALUE, MAX_VALUE);
+    const rightAnswer = isPrime(number) ? 'yes' : 'no';
+    return [number, rightAnswer];
+  };
 
-    runEngine(getDataForRound, question);
+  runEngine(getDataForRound, question);
 };
 
 export default runPrimeGame;
